e7ba74a4ee55c3e735894a5e3980e5ffdc8663b9
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-26 10:46:13 -0300

    Changing styles

--- src/components/mainPages/presentation/Presentation.jsx
+++ src/components/mainPages/presentation/Presentation.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./presentation.css";
 
@@ -21,25 +20,3 @@ function Presentation(props) {
 }
 
 export default Presentation;
-=======
-import React from "react";
-import "./presentation.css";
-
-import Model from "../../utils/Model/Model";
-import ExampleModel from "../../utils/ExampleModel/ExampleModel";
-
-function Presentation(props) {
-  return (
-    <section className="presentation-section">
-      {/* <Model /> */}
-      {/* <ExampleModel/>  */}
-
-       <h1 class="title">CB</h1>
-      <div class="separator"></div>
-      <h2 class="subtitle">Desarrollo de Software</h2> 
-    </section>
-  )
-}
-
-export default Presentation;
->>>>>>> 33e23e6fec986364e78a1762dda785d5ad147cd8
