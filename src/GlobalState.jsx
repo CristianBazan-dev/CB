afafc2c42fee24e6b036ce019142c71c02e391e3
Cristian Bazan <johanwebdesigner@gmail.com>
2023-09-26 11:00:32 -0300

    Deleting all

--- src/GlobalState.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from "react";
-
-export const GlobalState = createContext();
-
-export const DataProvider = ({ children }) => {
-  const state = {};
-
-  return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
-};
