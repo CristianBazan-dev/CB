d4cf38b2410cc5d24203fcc58f26c33d42499f5d
Cristian Bazan <johanwebdesigner@gmail.com>
2023-10-02 16:44:43 -0300

    Changing base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "https://cristianbazan-dev.github.io/CB/"
+  base: "https://cristianbazan-dev.github.io"
 });
